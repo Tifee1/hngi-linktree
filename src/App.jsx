4896639fdb7cf8bd24c7c9c9a79876506862ff35
Tifee <boludami238@gmail.com>
2022-11-03 09:14:17 +0100

    feat-added form

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Contact from './Contact'
 import Footer from './Footer'
 import Home from './Home'
+import { ToastContainer } from 're'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path='/contact' element={<Contact />} />
       </Routes>
       <Footer />
+      <ToastContainer />
     </BrowserRouter>
   )
 }
